275102940ce098842613f05206ec597498efaaaa
samet <samet.mollaoglu85@gmail.com>
2024-02-23 12:47:19 +0300

    search functionality

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -10,4 +10,6 @@ import org.springframework.data.repository.query.Param;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
