1711f30687a5db6b1f2375ddb73584e2ddc34a63
samet <samet.mollaoglu85@gmail.com>
2024-03-22 15:38:33 +0300

    tomcat configurations

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -2,9 +2,10 @@ package com.luv2code.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootEcommerceApplication {
+public class SpringBootEcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
